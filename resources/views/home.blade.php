7dea8cda85d8ca769b4caaa2f584efad67517014
Marco TB <alvinmarco8@gmail.com>
2024-03-12 15:50:19 +0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            
+        </div>
+        <div class="col-9">
+            <div><h1></h1></div>
+
+        </div>
+    </div>
+</div>
+@endsection
