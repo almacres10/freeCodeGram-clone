72bb1dc91b1034aa1f062e12dff395b366e338f5
Marco TB <alvinmarco8@gmail.com>
2024-03-17 14:32:46 +0700

    update

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -8,12 +8,28 @@
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{ $user->username }}</h1></div>
+                <h1>{{ $user->username }}</h1>
+                <a href="/p/create">Add New Post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong>posts</div>
+                <div class="pr-5"><strong>23k</strong>posts</div>
+                <div class="pr-5"><strong>212</strong>posts</div>
+            </div>
 
             <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
             <div>{{ $user->profile->description }}</div>
             <div><a href="#">{{ $user->profile->url ?? 'N/A'}}</a></div>
         </div>
     </div>
+
+    <div class="row pt-5">
+        @foreach ($user->posts as $post)
+        <div class="col-4 pb-4">
+            <img src="/storage/{{ $post->image }}" class="w-100" alt="">
+        </div>
+        @endforeach
+    </div>
+
 </div>
 @endsection
